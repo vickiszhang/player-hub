63df734cfdc9aca15726826db1fea5d6cc966d7c
davidlim <daviddlim11@gmail.com>
2023-09-27 22:12:22 -0700

    git commit message

---
+++ frontend/src/App.tsx
@@ -0,0 +1,27 @@
+import './App.css'
+import { createBrowserRouter } from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom'
+import Home from './routes/Home.tsx'
+import ErrorPage from './errors-page.tsx'
+import Profile from './routes/profile.tsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <ErrorPage />
+  },
+
+  {
+    path: "/profile",
+    element: <Profile />
+  }
+])
+
+export default function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  )
+}
---
+++ frontend/src/routes/Profile.tsx
@@ -0,0 +1,9 @@
+import { Form } from "react-router-dom"
+
+export default function Profile(){
+    return(
+        <Form method="post">
+            <div>This is the profile creator</div>
+        </Form>
+    )
+}
